3665b8f170f37592d0cd9d3e4db5a07209f233eb
MEEP of Faith <meepmatthew@gmail.com>
2020-11-17 22:43:23 -0800

    Colors

---
+++ scripts/mod-meta.js
@@ -0,0 +1,4 @@
+const meta = Vars.mods.locateMod("do-not-the-cat").meta;
+meta.displayName = "[red]Do not the Anuke"
+meta.author = "[#FCC21B]MEEP of Faith";
+meta.description = "[red]Do not the Anuke"
