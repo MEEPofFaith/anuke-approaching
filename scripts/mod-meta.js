feabe7e9e45f8c1cbf544373df6267b12d822927
MEEP of Faith <meepmatthew@gmail.com>
2020-11-18 23:34:41 -0800

    Update mod-meta.js

--- scripts/mod-meta.js
+++ scripts/mod-meta.js
@@ -1,4 +1,6 @@
-const meta = Vars.mods.locateMod("do-not-the-cat").meta;
-meta.displayName = "[red]Do not the Anuke"
-meta.author = "[#FCC21B]MEEP of Faith";
-meta.description = "[red]Do not the Anuke"
+if(!Vars.headless){
+  const meta = Vars.mods.locateMod("do-not-the-cat").meta;
+  meta.displayName = "[red]Do not the Anuke"
+  meta.author = "[#FCC21B]MEEP of Faith";
+  meta.description = "[red]Do not the Anuke"
+}
